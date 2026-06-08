11c92210a8b05c2d97715721bd6e37a07e2fd2f8
Hajar <hajar2000alfarsi@gmail.com>
2026-06-08 17:43:13 +0400

    Add fields for employee class

--- src/main/java/com/example/demoTask8/Entity/Employee.java
+++ src/main/java/com/example/demoTask8/Entity/Employee.java
@@ -1,4 +1,12 @@
 package com.example.demoTask8.Entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class Employee {
+    private String employeeId;
+    private String employeeName;
+    private String dDepartment;
 }
