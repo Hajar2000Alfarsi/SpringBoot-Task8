cc028252ed2604185285ae9509cae43f9944a42f
Hajar <hajar2000alfarsi@gmail.com>
2026-06-08 17:48:06 +0400

    Add employee method

--- src/main/java/com/example/demoTask8/Controller/EmployeeController.java
+++ src/main/java/com/example/demoTask8/Controller/EmployeeController.java
@@ -1,4 +1,7 @@
 package com.example.demoTask8.Controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class EmployeeController {
 }
--- src/main/java/com/example/demoTask8/Service/EmployeeService.java
+++ src/main/java/com/example/demoTask8/Service/EmployeeService.java
@@ -1,4 +1,21 @@
 package com.example.demoTask8.Service;
 
+import com.example.demoTask8.Entity.Employee;
+import jakarta.annotation.PostConstruct;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
 public class EmployeeService {
+    List<Employee> employeeList = new ArrayList<>();
+
+    @PostConstruct
+    public void addEmployee() {
+        employeeList.add(new Employee("E101","Ahmed","IT"));
+        employeeList.add(new Employee("E102","Fatma","HR"));
+        employeeList.add(new Employee("E103","Khawla","Operation"));
+    }
+
 }
