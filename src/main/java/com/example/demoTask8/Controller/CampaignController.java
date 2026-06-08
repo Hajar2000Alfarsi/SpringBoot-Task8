e83388ad724ec5c8dfa7dd18e65f2b65b04988f7
Hajar <hajar2000alfarsi@gmail.com>
2026-06-08 18:44:30 +0400

    Complete the task

--- src/main/java/com/example/demoTask8/Controller/CampaignController.java
+++ src/main/java/com/example/demoTask8/Controller/CampaignController.java
@@ -1,8 +1,30 @@
 package com.example.demoTask8.Controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.example.demoTask8.Entity.Campaign;
+import com.example.demoTask8.Service.CampaignManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class CampaignController {
+
+    @Autowired
+    CampaignManager campaignManager;
+
+    @PutMapping("addCampaign")
+    public String addCampaign(@RequestParam String campaignId,
+                              @RequestParam String campaignName,
+                              @RequestParam String platform,
+                              @RequestParam Double budget) {
+        Campaign campaign = new Campaign(campaignId, campaignName, platform, budget);
+        return campaignManager.addCampaign(campaign);
+    }
+
+    @GetMapping("displayCampaign")
+    public List<Campaign> displayCampaign() {
+        return campaignManager.displayCampaign();
+    }
 
 }
