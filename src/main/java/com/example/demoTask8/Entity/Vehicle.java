a61092914baf8ce3c73670c3f9b919e1aaba4d7b
Hajar <hajar2000alfarsi@gmail.com>
2026-06-08 18:16:18 +0400

    Add vehicle

--- src/main/java/com/example/demoTask8/Entity/Vehicle.java
+++ src/main/java/com/example/demoTask8/Entity/Vehicle.java
@@ -1,5 +1,10 @@
 package com.example.demoTask8.Entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class Vehicle {
     private String vehicleId;
     private String vehicleModel;
--- src/main/java/com/example/demoTask8/Service/VehicleManager.java
+++ src/main/java/com/example/demoTask8/Service/VehicleManager.java
@@ -1,8 +1,41 @@
 package com.example.demoTask8.Service;
 
+import com.example.demoTask8.Entity.Vehicle;
+import jakarta.annotation.PostConstruct;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class VehicleManager {
+    List<Vehicle> vehicleList = new ArrayList<>();
+
+    @PostConstruct
+    public void AddData(){
+        vehicleList.add(new Vehicle("V101", "Toyota Corolla", 20.00));
+        vehicleList.add(new Vehicle("V102", "Nissan Sunny", 18.00));
+        vehicleList.add(new Vehicle("V103", "Hyundai Elantra", 22.00));
+    }
+
+    public String AddVehicle(Vehicle vehicle) {
+        for (Vehicle v : vehicleList) {
+            if (v.getVehicleId().equals(vehicle.getVehicleId())) {
+                return "Vehicle ID already exists \n" +
+                        "No vehicle was added";
+            }
+
+        }
+
+        vehicleList.add(vehicle);
+        return "****************************\n" +
+                "Vehicle Added Successfully\n"
+                + "Vehicle ID: " + vehicle.getVehicleId() + "\n"
+                + "Vehicle Model: " + vehicle.getVehicleModel() + "\n"
+                + "Rental Price Per Day: " + vehicle.getRentalPricePerDay() + " OMR\n"
+                + "Status: Created\n"+
+                "***************************\n";
+    }
+
 
 }
