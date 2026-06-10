3c112f5e8b875b014eb142d0733b90da5e6afa82
Hajar <hajar2000alfarsi@gmail.com>
2026-06-10 21:28:39 +0400

    CRUD in Controller

--- src/main/java/com/example/demoTask8/Controller/EmployeeController.java
+++ src/main/java/com/example/demoTask8/Controller/EmployeeController.java
@@ -4,20 +4,42 @@ import com.example.demoTask8.Entity.Employee;
 import com.example.demoTask8.Repository.EmployeeRepository;
 import com.example.demoTask8.Service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class EmployeeController {
     @Autowired
     EmployeeService employeeService;
 
-    @PutMapping("addEmployee")
-    public Employee addEmployee(Employee employee) {
+    @PostMapping("add")
+    public Employee AddEmployee(@RequestBody Employee employee) {
         return employeeService.addEmployee(employee);
     }
 
+    @GetMapping("getAll")
+    public List<Employee> getAllEmployee(){
+        return employeeService.getAllEmployee();
+    }
 
+    @GetMapping("byId")
+    public Employee getEmployeeById(@RequestParam Integer id) {
+        return employeeService.getEmployeeById(id);
+    }
 
+    @GetMapping("byName")
+    public Employee getEmployeeByName(@RequestParam String name) {
+        return employeeService.getEmployeeByName(name);
+    }
+
+    @PutMapping("Update")
+    public Employee updateEmployee(@RequestBody Employee employee) throws Exception{
+        return employeeService.updateEmployee(employee);
+    }
+
+    @DeleteMapping("Delete")
+    public Boolean deleteEmployee(@RequestParam Integer id) {
+        return employeeService.deleteEmployeeById(id);
+    }
 }
